7bfa30328eb56d45ff16d4d58c92642c2ab58d7f
+ <Peleanr1989@gmail.com>
2021-11-20 18:59:22 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+const names= ["Lisa","Kaitlin","Jan","Birthday"];
+const cards =[];
+
+
+function writeCards(names, event) {
+    for (let a = 0; a < names.length; a++) {
+        cards.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`);
+    }
+return cards;
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
